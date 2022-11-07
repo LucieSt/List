3cd65abb8e313113dc2868a6d48c87b07711b7c2
LucieSt <strakova.91@gmail.com>
2022-11-07 16:17:14 +0100

    init

--- ShoppingItem/index.js
+++ ShoppingItem/index.js
@@ -1,16 +1,43 @@
 export const ShoppingItem = (props) => {
-  const { done, product, amount } = props;
+  const { id, product, amount, unit, done } = props;
 
   let checkClass = '';
   if (done) {
     checkClass = 'item__btn-done--check';
   }
 
-  return `
-    <li class="item">
-      <div class="item__name">${product}</div>
-      <div class="item__amount">${amount}</div>
-      <button class="item__btn-done ${checkClass}"></button>
-    </li>
+  const element = document.createElement('li');
+  element.classList.add('item');
+  element.innerHTML = `
+    <div class="item__name">${product}</div>
+    <div class="item__amount"><input type="number" value=${amount}></input> ${unit}</div>
+    <button class="item__btn-done ${checkClass}"></button>
   `;
+
+  const inputElm = element.querySelector('input');
+  inputElm.addEventListener('change', () => {
+    console.log(inputElm.value);
+    const newAmount = parseInt(inputElm.value);
+    fetch(`https://apps.kodim.cz/daweb/shoplist/api/weeks/6/days/mon/${id}`, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ amount: newAmount }),
+    }).then((response) => response.json())
+      .then((data) => element.replaceWith(ShoppingItem(data.results)));
+  });
+
+  element.querySelector('button').addEventListener('click', () => {
+    fetch(`https://apps.kodim.cz/daweb/shoplist/api/weeks/6/days/mon/${id}`, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ done: !done }),
+    }).then((response) => response.json())
+      .then((data) => element.replaceWith(ShoppingItem(data.results)));
+  });
+
+  return element;
 };
--- ShoppingList/index.js
+++ ShoppingList/index.js
@@ -1,13 +1,29 @@
 import { ShoppingItem } from "../ShoppingItem/index.js";
 
 export const ShoppingList = (props) => {
-  const { day, items } = props;
-  return `
-    <div class="shopping-list">
-      <h2>${day}</h2>  
-      <ul class="shopping-list__items">
-        ${items.map((item) => ShoppingItem(item)).join('')}
-      </ul>
-    </div>
+  const { day, dayName, items } = props;
+
+  const element = document.createElement('div');
+  element.classList.add('shopping-list');
+  element.innerHTML = `
+    <h2>${dayName}</h2>
+    <ul class="shopping-list__items"></ul>
   `;
+  
+  if (items === undefined) {
+    fetch(`https://apps.kodim.cz/daweb/shoplist/api/weeks/6/days/${day}`)
+      .then((response) => response.json())
+      .then((data) => {
+        element.replaceWith(ShoppingList({
+          day: day,
+          dayName: dayName,
+          items: data.results,
+        }));
+      });
+  } else {
+    const ulElement = element.querySelector('ul');
+    ulElement.append(...items.map((item) => ShoppingItem(item)));
+  }
+
+  return element;
 };
